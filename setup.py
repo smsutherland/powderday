b59ad0f71ab8630b4763d9b6def3aa9d445ab4e2
Sagan Sutherland <sagan.sutherland@uconn.edu>
2025-06-12 13:09:52 -0400

    automatic installation of vendored dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,59 @@
 from setuptools import setup, find_packages
+from importlib.util import find_spec
+from sys import argv
+import os
 
-try:
-    import hyperion
-except ImportError:
-    raise Exception("Hyperion must be installed manually, see docs/installation.rst")
+if "--fsps" in argv:
+    argv.remove("--fsps")
+
+    os.system(
+        "git submodule update --recursive --init -- vendor/python-fsps"
+    )  # Download fsps submodule if it doesn't exist. Does nothing if it does.
+    os.chdir("vendor/python-fsps/src/fsps/libfsps/src")
+    os.system("make clean")
+    os.system("make")
+    os.chdir("../../../..")
+    os.system("python setup.py install")
+    os.chdir("../..")
+
+if "--hyperion" in argv:
+    argv.remove("--hyperion")
+
+    os.system("git submodule update --recursive --init -- vendor/hyperion")
+    os.chdir("vendor/hyperion")
+    os.system("python setup.py install")
+    os.system(
+        "./configure --prefix=$HOME/local"
+    )  # Do we really want to hardcode $HOME/local?
+    os.system("make")
+    os.system("make install")
+    os.chdir("../..")
+
+    os.system("git submodule update --recursive --init -- vendor/hyperion-dust")
+    os.chdir("vendor/hyperion-dust")
+    os.system("python setup.py build_dust")
+    os.chdir("dust_files")
+    os.system(
+        "wget https://github.com/hyperion-rt/paper-galaxy-rt-model/blob/master/dust/big.hdf5"
+    )
+    os.system(
+        "wget https://github.com/hyperion-rt/paper-galaxy-rt-model/blob/master/dust/vsg.hdf5"
+    )
+    os.system(
+        "wget https://github.com/hyperion-rt/paper-galaxy-rt-model/blob/master/dust/usg.hdf5"
+    )
+    os.chdir("../../..")
+
+
+if find_spec("fsps") is None:
+    raise Exception(
+        "pass `--fsps` or install python-fsps manually, see docs/installation.rst"
+    )
+
+if find_spec("hyperion") is None:
+    raise Exception(
+        "pass `--hyperion` or install Hyperion manually, see docs/installation.rst"
+    )
 
 setup(
     name="powderday",
@@ -16,18 +66,17 @@ setup(
     ],
     install_requires=[
         'scipy==1.2; python_version=="2.7"',
-        'scipy==1.7.3; python_version>="3.5"',
+        'scipy==1.10; python_version>="3.5"',
         'astropy==2.0; python_version=="2.7"',
-        'astropy; python_version>="3.5"',
-        'h5py>=2.9',
-        'yt',
-        'unyt',
-        'fsps',
-        'scikit-learn',
-        'p_tqdm'
+        'astropy==6; python_version>="3.5"',
+        "h5py==3.14.0",
+        "yt==4.4.0",
+        "unyt==3.0.4",
+        "scikit-learn==1.6.1",
+        "p_tqdm==1.4.2",
     ],
     scripts=["pd_front_end.py"],
     project_urls={
-        'Source': 'https://github.com/dnarayanan/powderday.git',
+        "Source": "https://github.com/dnarayanan/powderday.git",
     },
 )
